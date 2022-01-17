a7f09a701064d51f335182d748d367010deb69db
Caleb-Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2022-01-16 18:33:55 -0600

    label page remove unnessary

--- src/routes/labels.svelte
+++ src/routes/labels.svelte
@@ -2,31 +2,26 @@
 	import { tagsStore } from '$lib/tagsStore';
 	import JsBarcode from 'jsbarcode';
 	import { onMount } from 'svelte';
-	// import LabelPage from '$lib/LabelPage.svelte';
 	import SvgLabelPage from '$lib/SvgLabelPage.svelte';
 	import divideArray from '$lib/divideArray';
 
-	let width = 3,
-		height = 10,
-		svgStyle = '',
-		tags = divideArray($tagsStore, width * height < 1 ? 1 : width * height);
-	const loadBarcodes = () => {
-		svgStyle = '';
-		JsBarcode('.barcode').init();
-		svgStyle = 'transform: scale(0.8) translate(0, -66%);';
-		JsBarcode('.barcode-svg').init();
-		document.querySelectorAll('.barcode-svg').forEach((svg: HTMLElement) => {
-			svg.style.transform = 'scale(0.4) translate(125px, -75px)';
-		});
-	};
+	const tags = divideArray($tagsStore, 30),
+		loadBarcodes = () => {
+			JsBarcode('.barcode').init();
+			JsBarcode('.barcode-svg').init();
+			document.querySelectorAll('.barcode-svg').forEach((svg: HTMLElement) => {
+				svg.style.transform = 'scale(0.4) translate(125px, -75px)';
+			});
+		};
 	onMount(loadBarcodes);
 
 	let html2pdf = null;
 	const genPDF = async () => {
+		loading = true;
 		if (html2pdf === null) {
 			html2pdf = await import('html2pdf.js');
 		}
-		html2pdf
+		await html2pdf
 			.default()
 			.set({
 				pagebreak: { mode: 'css' },
@@ -41,32 +36,33 @@
 			.error((e) => {
 				console.log('Error logged: ', e);
 			});
+		loading = false;
 	};
+
+	let debug = false,
+		loading = false;
 </script>
 
 <h1>Labels</h1>
 <div class="border-solid border-black border-2 rounded-md p-1 m-1 bg-white grid text-center">
-	<label for="Height">Height</label>
-	<input
-		type="number"
-		name="Height"
-		id=""
-		bind:value={height}
-		class="bg-slate-300 p-1 text-center"
-	/>
-	<label for="Width">Width</label>
-	<input type="number" name="Width" id="" bind:value={width} class="bg-slate-300 p-1 text-center" />
-	= Labels {width * height} per page ({$tagsStore.length} labels, {Math.ceil(
-		$tagsStore.length / (width * height)
-	)} total pages)
+	30 labels per page, {$tagsStore.length} label{$tagsStore.length !== 1 ? 's' : ''}, {Math.ceil(
+		$tagsStore.length / 30
+	)} total page{Math.ceil($tagsStore.length / 30) !== 1 ? 's' : ''}
 	<br />
-	<button class="border-solid border-black border-2 rounded-md p-1 m-1 bg-white" on:click={genPDF}
-		>Generate PDF</button
-	>
 	<button
-		class="border-solid border-black border-2 rounded-md p-1 m-1 bg-white"
-		on:click={loadBarcodes}>Rerender Barcodes</button
+		class="border-solid border-black border-2 rounded-md p-1 m-1 bg-white disabled:cursor-wait"
+		disabled={loading}
+		on:click={genPDF}
+		>{#if !loading} Generate PDF {:else} <p class="animate-ping">◯</p> {/if}</button
 	>
+	This may take a long time (more then 30 seconds)
+	<button on:click={() => (debug = !debug)}>Debug</button>
+	{#if debug}
+		<button
+			class="border-solid border-black border-2 rounded-md p-1 m-1 bg-white"
+			on:click={loadBarcodes}>Rerender Barcodes (don't press while generating pdf)</button
+		>
+	{/if}
 </div>
 
 <div class="border-2 border-solid border-black flex justify-center">
@@ -78,9 +74,3 @@
 		{/each}
 	</div>
 </div>
-
-<!-- <div>
-	{#each tags as page}
-		<LabelPage {...{ page, width, svgStyle }} />
-	{/each}
-</div> -->
