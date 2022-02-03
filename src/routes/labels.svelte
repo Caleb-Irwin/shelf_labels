62f93c892f4eca7bfd727af57bae77579f5af1c1
Caleb-Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2022-02-02 18:34:58 -0600

    Don't wait to render if on first page

--- src/routes/labels.svelte
+++ src/routes/labels.svelte
@@ -59,7 +59,10 @@
 			const prevOffset = pageOffset;
 			pageOffset = 0;
 			while (pageOffset <= Math.ceil($tagsStore.length / 30 / 4) - 1) {
-				if (Math.ceil($tagsStore.length / 30 / 4) !== 1) {
+				if (
+					Math.ceil($tagsStore.length / 30 / 4) !== 1 &&
+					!(pageOffset === 0 && prevOffset === 0)
+				) {
 					await wait(3000);
 				}
 				const ref = document.querySelectorAll('.label-page-holder');
